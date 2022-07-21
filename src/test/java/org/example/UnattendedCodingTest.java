ceea6b44b63dfd2e357095b368dbcc8431556fa8
Manuel Canadas <canadasm@assurity.local>
2022-07-21 12:10:19 +1200

    Add test for promotion with gallery matching description

--- src/test/java/org/example/UnattendedCodingTest.java
+++ src/test/java/org/example/UnattendedCodingTest.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import io.restassured.path.json.JsonPath;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -13,7 +18,6 @@ public class UnattendedCodingTest extends BaseTest {
                 .when()
                    .get("/Categories/6327/Details.json?catalogue=false")
                 .then()
-                   //.log().all()
                    .statusCode(200)
                    .body("Name", equalTo("Carbon credits"));
 
@@ -31,13 +35,15 @@ public class UnattendedCodingTest extends BaseTest {
     }
     @Test
     public void ValidateGallery() {
+        String name = "Gallery";
 
-        given()
-                .when()
-                .get("/Categories/6327/Details.json?catalogue=false")
-                .then()
-                .statusCode(200)
-                .body("Promotions[1].Description", equalTo("Good position in category"));
+        JsonPath jsonPath = given().when().get("/Categories/6327/Details.json?catalogue=false").jsonPath();
+
+        Map<String, Object> promotion = jsonPath.param("name", name).get("Promotions.find { p -> p.Name == name }");
+
+        Assert.assertNotNull(promotion);
+
+        Assert.assertEquals(promotion.get("Description"),  "Good position in category");
 
     }
 }
